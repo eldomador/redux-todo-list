5dd19c75bf22368bd7bbfcd7b0521b2fb57d4ff9
eldomador <johncenajakub@gmail.com>
2022-04-29 12:20:23 +0200

    feat: adding css and provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <CssBaseline />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
-
